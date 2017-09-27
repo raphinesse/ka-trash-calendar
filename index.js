a75604f35d42c229bfdabdb7d1717cf927ac62cb
Raphael von der Grün <raphinesse@gmail.com>
2017-09-27 16:18:33 +0200

    Add caching of requests

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const cache = require('apicache').middleware
 const port = require('./port')
 const retrieveTrashCalendar = require('./retrieve-trash-calendar')
 
 const app = express()
 
-app.get('/:street', function (req, res) {
+app.get('/:street', cache('1 week'), (req, res) => {
   const street = req.params.street
   const fileName = `Abfallkalender ${street}.ics`
   retrieveTrashCalendar(req.params.street)
